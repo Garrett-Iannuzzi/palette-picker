0d5b511d5a505af140e0c4710b303833927590b3
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-02-03 15:29:01 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (request, response) => {
+  response.send('Oh hey Pet Box');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
