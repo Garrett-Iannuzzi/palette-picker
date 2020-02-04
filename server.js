ea1aff413e2f7f56db38a9fa36b9afd4ee4bbe90
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-02-04 13:18:47 -0700

    Add GET request for all projects

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const app = express();
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+
 app.use(express.json());
 
 
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (request, response) => {
-  response.send('Oh hey Pet Box');
+  response.send('Reached Palette Picker');
+});
+
+app.get('/api/v1/projects', async (request, response) => {
+  try {
+    const projects = await database('projects').select();
+    return response.status(200).json(projects);
+  } catch (error) {
+    return response.status(500).json({ error });
+  }
+});
+
+app.get('/', (request, response) => {
+  response.send('Reached Palette Picker');
+});
+
+app.get('/', (request, response) => {
+  response.send('Reached Palette Picker');
 });
 
 app.listen(app.get('port'), () => {
