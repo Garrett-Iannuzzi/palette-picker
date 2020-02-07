9af240788a0729085247fe42776a6bc7aa138e46
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-02-07 12:07:54 -0700

    Add no emails to yml file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = require('./app');
 
 app.set('port', process.env.PORT || 3000);
 
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 });
